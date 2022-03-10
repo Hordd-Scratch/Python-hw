532daf22be42ffe9023c632ddf9155893aa56b4c
Horrd <hordd.scratch@gmail.com>
2022-03-11 01:22:16 +1000

    I hate hw3

---
+++ HomeWork2/hw3.py
@@ -0,0 +1,33 @@
+"""
+Write a function that takes K lists as arguments and returns all possible
+lists of K items where the first element is from the first list,
+the second is from the second and so one.
+
+You may assume that that every list contain at least one element
+
+Example:
+
+assert combinations([1, 2], [3, 4]) == [
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+]
+"""
+from typing import List, Any
+
+
+def combinations(args: List[Any]) -> List[List]:
+    a = iter(args)
+    part = object()
+    res = [[]]
+    while True:
+        temp = next(a, part)
+        if temp == part:
+            break
+        res = [r + [i] for r in res for i in temp]
+    return res
+
+
+abc = [[1, 2], [3, 4], [5, 6]]
+print(combinations(abc))
