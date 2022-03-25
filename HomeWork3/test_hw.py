734d5730086367c2d1f1cf2e74751218e9928c4b
Horrd <hordd.scratch@gmail.com>
2022-03-25 11:36:59 +1000

    update

---
+++ HomeWork3/test_hw.py
@@ -0,0 +1,15 @@
+# import pytest
+# import time
+
+
+# @pytest.mark.parametrize("value",[(100,40000)])
+# def test cashe(value: int)
+#     first_time = time.time()
+#     actual_result = fun(*value)
+#     print(f"\nFirst call: {str(time.time() - first_time)}")
+#     second_time = time.time()
+#     expected_result = fun(*value)
+#     print(f"Second call: {format(str(time.time() - second_time))}")
+#     assert actual_result is expected_result
+
+# IDK
