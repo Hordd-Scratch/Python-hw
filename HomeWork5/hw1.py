d0d78c4c105297f7e2439553e0246348115bbc2f
Horrd <hordd.scratch@gmail.com>
2022-03-25 14:06:22 +1000

    update

---
+++ HomeWork5/hw1.py
@@ -0,0 +1,15 @@
+import datetime
+from abc import ABC
+
+
+class Homework(ABC):
+    def __init__(self, text: str, deadline: datetime.timedelta):
+        self.text = text
+        self.deadline = deadline
+        self.created = datetime.datetime.now()
+
+    def is_active(self):
+        return self.created + self.deadline >= datetime.datetime.now()
+
+
+
