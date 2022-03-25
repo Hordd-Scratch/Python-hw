725bf51445a857c7843efe3f92e551439307d295
Horrd <hordd.scratch@gmail.com>
2022-03-25 11:06:35 +1000

    update

--- HomeWork4/test_hw_hz.py
+++ HomeWork4/test_hw_hz.py
@@ -2,6 +2,15 @@ import pytest
 from hw_hz import is_armstrong
 
 
-@pytest.mask.parametrise("a, expected", [(1, 1)])
-def test_is_armstrong(a):
-    return
+@pytest.mark.parametrize("a, expected_result",
+                         [(9, True),
+                          (153, True)])
+def test_is_armstrong_good(a, expected_result):
+    assert is_armstrong(a) == expected_result
+
+
+@pytest.mark.parametrize("a, expected_result",
+                         [(10, False),
+                          (152, False)])
+def test_is_armstrong_bad(a, expected_result):
+    assert is_armstrong(a) == expected_result
