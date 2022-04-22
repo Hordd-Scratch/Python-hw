e0c39f94ae1bdb800855f4b2491e3e1783a52f80
Horrd <hordd.scratch@gmail.com>
2022-04-22 11:03:29 +1000

    update

--- HomeWork5/test_hw1.py
+++ HomeWork5/test_hw1.py
@@ -8,28 +8,7 @@ from hw1 import Teacher, Student
                           ('zxcvb', 'zxcvb'),
                           ('amogus', 'amogus'),
                           ('Vova', 'Vova')])
-def Teachers_last_name_good(a, expected_result):
-    teacher = Teacher(a, 'Shadrin')
-    assert teacher.last_name == expected_result
-
-
-@pytest.mark.parametrize("a, expected_result",
-                         [('q5werty', 'qwerty'),
-                          ('a5sdfg', 'asdfg'),
-                          ('z5xcvb', 'zxcvb'),
-                          ('a5mogus', 'amogus'),
-                          ('V5ova', 'Vova')])
-def Teachers_last_name_bad(a, expected_result):
-    teacher = Teacher(a, 'Shadrin')
-    assert teacher.last_name != expected_result
-
-@pytest.mark.parametrize("a, expected_result",
-                         [('qwerty', 'qwerty'),
-                          ('asdfg', 'asdfg'),
-                          ('zxcvb', 'zxcvb'),
-                          ('amogus', 'amogus'),
-                          ('Vova', 'Vova')])
-def Teachers_first_name_good(a, expected_result):
+def test_Teachers_last_name_good(a, expected_result):
     teacher = Teacher('Shadrin', a)
     assert teacher.last_name == expected_result
 
@@ -40,7 +19,7 @@ def Teachers_first_name_good(a, expected_result):
                           ('z5xcvb', 'zxcvb'),
                           ('a5mogus', 'amogus'),
                           ('V5ova', 'Vova')])
-def Teachers_first_name_bad(a, expected_result):
+def test_Teachers_last_name_bad(a, expected_result):
     teacher = Teacher('Shadrin', a)
     assert teacher.last_name != expected_result
 
@@ -50,8 +29,30 @@ def Teachers_first_name_bad(a, expected_result):
                           ('zxcvb', 'zxcvb'),
                           ('amogus', 'amogus'),
                           ('Vova', 'Vova')])
-def Student_last_name_good(a, expected_result):
-    student = Student(a, 'Shadrin')
+def test_Teachers_first_name_good(a, expected_result):
+    teacher = Teacher(a, 'Shadrin')
+    assert teacher.first_name == expected_result
+
+
+@pytest.mark.parametrize("a, expected_result",
+                         [('q5werty', 'qwerty'),
+                          ('a5sdfg', 'asdfg'),
+                          ('z5xcvb', 'zxcvb'),
+                          ('a5mogus', 'amogus'),
+                          ('V5ova', 'Vova')])
+def test_Teachers_first_name_bad(a, expected_result):
+    teacher = Teacher(a, 'Shadrin')
+    assert teacher.first_name != expected_result
+
+
+@pytest.mark.parametrize("a, expected_result",
+                         [('qwerty', 'qwerty'),
+                          ('asdfg', 'asdfg'),
+                          ('zxcvb', 'zxcvb'),
+                          ('amogus', 'amogus'),
+                          ('Vova', 'Vova')])
+def test_Student_last_name_good(a, expected_result):
+    student = Student('Shadrin', a)
     assert student.last_name == expected_result
 
 
@@ -61,8 +62,8 @@ def Student_last_name_good(a, expected_result):
                           ('z5xcvb', 'zxcvb'),
                           ('a5mogus', 'amogus'),
                           ('V5ova', 'Vova')])
-def Student_last_name_bad(a, expected_result):
-    student = Student(a, 'Shadrin')
+def test_Student_last_name_bad(a, expected_result):
+    student = Student('Shadrin', a)
     assert student.last_name != expected_result
 
 @pytest.mark.parametrize("a, expected_result",
@@ -71,9 +72,9 @@ def Student_last_name_bad(a, expected_result):
                           ('zxcvb', 'zxcvb'),
                           ('amogus', 'amogus'),
                           ('Vova', 'Vova')])
-def Student_first_name_good(a, expected_result):
-    student = Student('Shadrin', a)
-    assert student.last_name == expected_result
+def test_Student_first_name_good(a, expected_result):
+    student = Student(a, 'Shadrin')
+    assert student.first_name == expected_result
 
 
 @pytest.mark.parametrize("a, expected_result",
@@ -82,8 +83,8 @@ def Student_first_name_good(a, expected_result):
                           ('z5xcvb', 'zxcvb'),
                           ('a5mogus', 'amogus'),
                           ('V5ova', 'Vova')])
-def Student_first_name_bad(a, expected_result):
-    student = Student('Shadrin', a)
-    assert student.last_name != expected_result
+def test_Student_first_name_bad(a, expected_result):
+    student = Student(a, 'Shadrin')
+    assert student.first_name != expected_result
 
 
